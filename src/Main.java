43a503f3b27a031c74a5e7201f8eda744c1ab17b
Trevor.Hensel@igrafx.com <Trevor.Hensel@igrafx.com>
2023-12-02 13:56:16 -0800

    Finished Day2Part2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import challenges.Day1Part1;
 import challenges.Day1Part2;
 import challenges.Day2Part1;
+import challenges.Day2Part2;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,5 +9,6 @@ public class Main {
         Day1Part1.getSolution();
         Day1Part2.getSolution();
         Day2Part1.getSolution();
+        Day2Part2.getSolution();
     }
 }
